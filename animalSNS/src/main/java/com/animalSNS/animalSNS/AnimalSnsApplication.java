f283a464d1faf53686935857c95245be1f089dfe
jeon hyebeen <hyebin199953@gmail.com>
2023-12-11 19:22:11 +0900

    springBoot

---
+++ animalSNS/src/main/java/com/animalSNS/animalSNS/AnimalSnsApplication.java
@@ -0,0 +1,13 @@
+package com.animalSNS.animalSNS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalSnsApplication.class, args);
+	}
+
+}
---
+++ animalSNS/src/test/java/com/animalSNS/animalSNS/AnimalSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animalSNS.animalSNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
